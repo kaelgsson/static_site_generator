9fa006f548d93d4f89b140a110ff0747cb808bc2
kaelgsson <mikael.s.gustafsson@proton.me>
2026-01-07 18:18:47 +0100

    ch_2_nodes

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+def main():
+    #just some initial test code.
+    new_node = TextNode("this is some dummy text", TextType.TEXT)
+    print(new_node)
+
+
+if __name__ == "__main__":
+    main()
