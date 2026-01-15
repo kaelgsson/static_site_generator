0d82880a5028ad0193c6172a33d3b1e9638f0bb5
kaelgsson <mikael.s.gustafsson@proton.me>
2026-01-15 22:41:07 +0100

    remove duplicate test

--- src/test_utils_blocks.py
+++ src/test_utils_blocks.py
@@ -77,21 +77,19 @@ This is another paragraph with _italic_ text and `code` here
             "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
         )
 
-    def test_paragraphs(self):
+    def test_codeblock(self):
         md = """
-This is **bolded** paragraph
-text in a p
-tag here
-
-This is another paragraph with _italic_ text and `code` here
-
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
 """
 
         node = markdown_to_html_node(md)
         html = node.to_html()
         self.assertEqual(
             html,
-            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
         )
 
     def test_lists(self):
