6a2a580f794184cfee3ebddb9db73bd8c600ab2c
kaelgsson <mikael.s.gustafsson@proton.me>
2026-01-15 23:10:56 +0100

    ch:5 l:1

--- src/main.py
+++ src/main.py
@@ -1,10 +1,26 @@
 from textnode import TextNode, TextType
+import os
+import shutil
 
+def copy_files_recursive(source_path, dest_path):
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
+
+    for filename in os.listdir(source_path):
+        file_from = os.path.join(source_path, filename)
+        file_too = os.path.join(dest_path, filename)
+        print(f"[{file_from}] move to: [{file_too}]")
+        if os.path.isfile(file_from):
+            shutil.copy(file_from, dest_path)
+        else:
+            copy_files_recursive(file_from, file_too)
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    print('deleting public folder')
+    if os.path.exists('./public'):
+        shutil.rmtree('./public')
 
+    copy_files_recursive('./static', './public')
 
 if __name__ == "__main__":
     main()
